0ec7156487b0069d8986ed8d20bd76b9f954fe45
Jonathan McKnight <themcknightrider@gmail.com>
2020-06-05 14:48:19 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -10,7 +10,6 @@ def key_for_min_value(name_hash)
   end
   empty_hash
 end
-a.sort_by { |key, value| value }.first
 ikea = {:chair => 25, :table => 85, :mattress => 450}
 key_for_min_value(ikea)
 # => :chair
